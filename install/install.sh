053fcefc5492c6c7cf1216f8c91c98593a44e808
Ludovic Meurillon <ludovic.meurillon@gmail.com>
2014-10-03 17:31:24 +0200

    add pulling tasks

--- install/install.sh
+++ install/install.sh
@@ -2,11 +2,11 @@
 
 if [ ! -d /usr/local/docker ]; then
   echo "create /usr/local/docker"
-  mkdir /usr/local/docker
+  sudo mkdir /usr/local/docker
 fi
 if [ ! -d /usr/local/docker ]; then
   echo "link /var/lib/docker to /usr/loca/docker"
-  ln -sf /usr/local/docker /var/lib/docker
+  sudo ln -sf /usr/local/docker /var/lib/docker
 fi
 
 export http_proxy=http://proxy:8080
@@ -19,5 +19,18 @@ git clone https://github.com/ludomeurillon/ODD2014.git
 cd ODD2014
 git checkout -t origin/master
 cd install
-sh install-docker-1.2.0-proxy.sh
+sudo ./install-docker-1.2.0-proxy.sh
+cd ~/ODD2014
+./build.sh
+
+
+#Prepare docker workshop
+sudo docker pull ubuntu:14.04
+sudo docker pull tutum/tomcat:7.0
+sudo docker pull mysql:5.6
+sudo docker pull registry:latest
+sudo docker pull google/cadvisor:latest
+
+cd ~/ODD2014
+./build.sh
 cd $pwd
