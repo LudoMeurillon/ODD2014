e3fd257f727e1e380e483cee67928fe45d69e836
Ludovic Meurillon <ludovic.meurillon@gmail.com>
2014-10-06 21:57:15 +0200

    add steps description

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
-sudo docker run -d --name machine5  devday/ansible-vm
-sudo docker run -d --name machine1  -p 5001:80 --link machine5:logserver devday/ansible-vm
-sudo docker run -d --name machine2  -p 5002:80 --link machine5:logserver devday/ansible-vm
-sudo docker run -d --name machine3  -p 5003:80 --link machine5:logserver devday/ansible-vm
-sudo docker run -d --name machine4  -p 5004:80 --link machine5:logserver devday/ansible-vm
+sudo docker run -d --name machine5  -h machine5 devday/ansible-vm
+sudo docker run -d --name machine1  -h machine1 -p 5001:80 --link machine5:logserver devday/ansible-vm
+sudo docker run -d --name machine2  -h machine2 -p 5002:80 --link machine5:logserver devday/ansible-vm
+sudo docker run -d --name machine3  -h machine3 -p 5003:80 --link machine5:logserver devday/ansible-vm
+sudo docker run -d --name machine4  -h machine4 -p 5004:80 --link machine5:logserver devday/ansible-vm
 
 sudo docker run -it --rm --name ansible -v `pwd`/devday-ansible-master/playbooks:/data --link machine1:machine1 --link machine2:machine2 --link machine3:machine3 --link machine4:machine4 --link machine5:machine5 -e ANSIBLE_SSH_ARGS="-F /root/.ssh/config" devday/ansible-master
